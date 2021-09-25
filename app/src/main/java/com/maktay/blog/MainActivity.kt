975c1c89a647b639d1f94b0d5032fe59262311ba
Murat Aktay <murataktay@Murats-MacBook-Pro.local>
2021-09-25 10:06:11 +0300

    bottom navigation bar added

--- app/src/main/java/com/maktay/blog/MainActivity.kt
+++ app/src/main/java/com/maktay/blog/MainActivity.kt
@@ -2,10 +2,29 @@ package com.maktay.blog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initBottomNavigation();
+    }
+
+    private fun initBottomNavigation() {
+        val bottom = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottom.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    println("home selected")
+                    true
+                }
+                R.id.bookmark -> {
+                    println("bookmark selected")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
