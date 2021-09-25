917b0536d7517af72213884ce7b679bbeff1c6c6
Murat Aktay <murataktay@Murats-MacBook-Pro.local>
2021-09-25 09:44:05 +0300

    splash screen added.

---
+++ app/src/main/java/com/maktay/blog/SplashScreen.kt
@@ -0,0 +1,26 @@
+package com.maktay.blog
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.lang.Exception
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(2000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
